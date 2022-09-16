0d816a6e7532070f42e2628f5abccbf3c2167368
Elias Lehtinen <elias.lehtinen01@gmail.com>
2022-09-16 13:36:26 +0300

    Implement starting position, modify board drawing

--- reversi/reversi.go
+++ reversi/reversi.go
@@ -14,12 +14,18 @@ const (
 
 
 type Board struct {
+    tiles [Width][Height]Tile
 }
 
 func NewBoard() *Board {
-    return &Board{}
+    board := new(Board)
+    board.tiles[3][3] = White
+    board.tiles[4][4] = White
+    board.tiles[4][3] = Black
+    board.tiles[3][4] = Black
+    return board
 }
 
-func (board *Board) get(x, y int) Tile {
-    return Empty
+func (board *Board) Get(x, y int) Tile {
+    return board.tiles[x][y]
 }
--- ui.go
+++ ui.go
@@ -7,10 +7,23 @@ import (
 
 
 func Draw(b *reversi.Board) {
-    fmt.Printf("Board %dx%d\n", reversi.Width, reversi.Height)
+    fmt.Println("  abcdefgh")
+    fmt.Println(" +--------+")
     for rank := 0; rank < reversi.Height; rank++ {
-        fmt.Println("   ox   ")
+        rankNum := rank + 1
+        fmt.Printf("%d|", rankNum)
+        for file := 0; file < reversi.Width; file++ {
+            tile := "."
+            switch b.Get(file, rank) {
+            case reversi.Black:
+                tile = "x"
+            case reversi.White:
+                tile = "o"
+            }
+            fmt.Print(tile)
+        }
+        fmt.Printf("|%d\n", rankNum)
     }
-    fmt.Println("--------")
-    fmt.Println("ABCDEFGH")
+    fmt.Println(" +--------+")
+    fmt.Println("  abcdefgh")
 }
